6d81aa2293b863f2da6bc24c7b26d2007d027f33
Phummiphat1 <p.karthong@gmail.com>
2026-03-06 17:45:46 +0100

    Round 1 update
    create some author resource from book resource

---
+++ react-app/src/authors/components/AuthorList.tsx
@@ -0,0 +1,29 @@
+import { useEffect } from 'react'
+import { useAuthorProvider } from '../providers/useAuthorProvider'
+import { AuthorListItem } from './AuthorListItem'
+import { CreateAuthorModal } from './CreateAuthorModal'
+
+export function AuthorList() {
+  const { authors, loadAuthors, deleteAuthor, updateAuthor, createAuthor } =
+    useAuthorProvider()
+
+  useEffect(() => {
+    loadAuthors()
+  }, [])
+
+  return (
+    <>
+      <CreateAuthorModal onCreate={createAuthor} />
+      <div style={{ padding: '0 .5rem' }}>
+        {authors.map(author => (
+          <AuthorListItem
+            key={author.id}
+            author={author}
+            onDelete={deleteAuthor}
+            onUpdate={updateAuthor}
+          />
+        ))}
+      </div>
+    </>
+  )
+}
