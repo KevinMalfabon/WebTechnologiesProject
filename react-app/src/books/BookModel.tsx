044ddccf78f92c38c66111f94fdc6e1d2f711435
Dmytro Chekhovskyi <64646103+TheDeiw@users.noreply.github.com>
2026-03-07 16:24:58 +0100

    ADD: new design for the book details page, changed DTO for description and cover URL

--- react-app/src/books/BookModel.tsx
+++ react-app/src/books/BookModel.tsx
@@ -7,12 +7,16 @@ export type BookModel = {
     firstName: string
     lastName: string
   }
+  coverUrl?: string
+  description?: string
 }
 
 export type CreateBookModel = {
   authorId: string
   title: string
   yearPublished: number
+  coverUrl?: string
+  description?: string
 }
 
 export type UpdateBookModel = Partial<CreateBookModel>
